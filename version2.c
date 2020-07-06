d35a7401d0862349befc447687e9fbfb14b13815
Nicholas J. Reid <nreid.pa@protonmail.com>
2020-07-06 09:59:45 +0100

    On branch Exercise.02  Changes to be committed: 	new file:   version2.c

---
+++ version2.c
@@ -0,0 +1,31 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+void delay(int count);
+int main(void)
+{
+/* Enable the clock to PORT B */
+SIM_SCGC5 |= SIM_SCGC5_PORTB_MASK;
+/* Select the GPIO function (Alternative 1) for pin 21 of PORT B */
+PORTB_PCR21 &= ~PORT_PCR_MUX_MASK;
+PORTB_PCR21 |= (1u << PORT_PCR_MUX_SHIFT);
+/* Set the data direction for pin 21 of PORT B to output */
+GPIOB_PDDR |= PIN21_MASK;
+while (true) {
+/* Turn on the blue LED */
+GPIOB_PDOR &= ~PIN21_MASK;
+/* Wait for about 1 second */
+delay(1000);
+/* Turn off the blue LED */
+GPIOB_PDOR |= PIN21_MASK;
+/* Wait for about 1 second */
+delay(1000);
+}
+return 0;
+}
+void delay(int count) {
+volatile int i = 0;
+for (i = count*1000 ; i !=0; i--) {
+/* skip */
+}
+}
